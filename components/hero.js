61df76e486fbacfefaf6cac4ebe316b28e35e535
Sayantan Karmakar <sayantankarmakar@outlook.com>
2021-07-20 17:57:27 +0530

    hero responsive

--- components/hero.js
+++ components/hero.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="w-full h-80vh flex items-center justify-center overflow-hidden">
+    <div className="w-full md:min-h-80vh flex items-center justify-center overflow-hidden">
       <Image
         src="https://images.unsplash.com/photo-1599809275671-b5942cabc7a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
         layout="intrinsic"
